1d2b5100fcbba5d61506f7a0b03f05cdff5ae5a0
Phil Hanna <ph1204@gmail.com>
2023-03-17 21:07:08 -0400

    Added stub of createNewArticle

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -17,11 +19,16 @@ import (
 // Variables
 // ---------------------------------------------------------------------
 
-
-
 // ---------------------------------------------------------------------
 // Functions
 // ---------------------------------------------------------------------
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+    // get the body of our POST request
+    // return the string response containing the request body    
+    reqBody, _ := ioutil.ReadAll(r.Body)
+    fmt.Fprintf(w, "%+v", string(reqBody))
+}
 
 func homePage(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -35,7 +42,13 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 
 func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
 	log.Println("Endpoint Hit: returnSingleArticle")
-	json.NewEncoder(w).Encode(Articles)
+    vars := mux.Vars(r)
+    key := vars["id"]
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
 }
 
 func handleRequests() {
