20d8e2a4fc18a7b36b829e0514f1270ec10caba5
Phil Hanna <ph1204@gmail.com>
2023-03-17 20:27:13 -0400

    Added stuff

--- main.go
+++ main.go
@@ -1,21 +1,61 @@
+// Tutorial source is https://tutorialedge.net/golang/creating-restful-api-with-golang/
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 )
+
+// ---------------------------------------------------------------------
+// Type definitions
+// ---------------------------------------------------------------------
+
+type Article struct {
+    Title string `json:"Title"`
+    Desc string `json:"desc"`
+    Content string `json:"content"`
+}
+
+// ---------------------------------------------------------------------
+// Variables
+// ---------------------------------------------------------------------
+
+// let's declare a global Articles array
+// that we can then populate in our main function
+// to simulate a database
+var Articles []Article
+
+// ---------------------------------------------------------------------
+// Functions
+// ---------------------------------------------------------------------
 
 func homePage(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+    log.Println("Endpoint Hit: homePage")
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	log.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
 }
 
 func handleRequests() {
+	const HOST = "localhost"
+	const PORT = 10000
+
     http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":10000", nil))
+	http.HandleFunc("/articles", returnAllArticles)
+	hostAndPort := fmt.Sprintf("%s:%d", HOST, PORT)
+	log.Printf("Starting server on port %d\n", PORT)
+    log.Fatal(http.ListenAndServe(hostAndPort, nil))
 }
 
 func main() {
+	Articles = []Article{
+        {Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+        {Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+    }
     handleRequests()
 }
