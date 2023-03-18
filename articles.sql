5ee51c93f3d49242e2ca97c57a89913567966a74
Phil Hanna <ph1204@gmail.com>
2023-03-17 23:02:09 -0400

    Fixed ID problem in database

--- articles.sql
+++ articles.sql
@@ -7,5 +7,5 @@ CREATE TABLE articles (
 );
 BEGIN;
 INSERT INTO articles VALUES("1", "Hello 1", "Article 1 description", "Article 1 content");
-INSERT INTO articles VALUES("1", "Hello 2", "Article 2 description", "Article 2 content");
+INSERT INTO articles VALUES("2", "Hello 2", "Article 2 description", "Article 2 content");
 COMMIT;
